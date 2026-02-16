ed3d00056bd161c236c692077fe15abb15dae9f6
GabrielSam26 <gabrielsamdev@gmail.com>
2026-02-16 13:46:20 -0300

    chore: atualizar scripts de build

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ if ($weatherId == 800 && $isDayTime) {
             extend: {
                 colors: {
                     'eyefind-blue': '#067191',
-                    'eyefind-light': '#EEF3F5',
+                    'eyefind-light': '#F8FAFC',
                     'eyefind-dark': '#02343F',
                     'eyefind-container': '#DCE7EB'
                 }
