745411b79c5b6f88ab05ca520bcbd96167a91f21
GabrielSam26 <gabrielsamdev@gmail.com>
2026-02-17 18:47:53 -0300

    chore: preparar ambiente para deploy

--- category.php
+++ category.php
@@ -83,7 +83,6 @@ $websitesDaCategoria = getWebsitesByCategoria($pdo, $categoria_id);
                     <!-- Voltar -->
                     <div class="relative group">
                         <a href="index.php" class="p-3 hover:scale-110 transition duration-200">
-                            <img src="icon/voltar.png" class="w-8 h-8" alt="Voltar">
                         </a>
 
                         <div class="absolute -bottom-10 left-1/2 -translate-x-1/2 
