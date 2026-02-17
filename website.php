bcef720d912900cccfc8f7d3b91333ba18605035
GabrielSam26 <gabrielsamdev@gmail.com>
2026-02-16 21:03:24 -0300

    feat: implementar pesquisa

--- website.php
+++ website.php
@@ -28,7 +28,6 @@ if ($website['status'] !== 'approved') {
 
 function sanitize_css($css)
 {
-    // Remove any header-related CSS
     $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
     $css = preg_replace('/#eyefind-header[^{]*{[^}]*}/', '', $css);
     $css = preg_replace('/header[^{]*{[^}]*}/', '', $css);
@@ -38,15 +37,10 @@ function sanitize_css($css)
     return $css;
 }
 
-$sanitized_css = sanitize_css($website['css_personalizado']);
-
-$htmlFinal = renderDynamicBlocks($website['conteudo'], $website['id'], $pdo);
-echo $htmlFinal;
+$sanitized_css = sanitize_css($website['css_personalizado'] ?? '');
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -54,28 +48,16 @@ echo $htmlFinal;
     <title><?php echo htmlspecialchars($website['nome']); ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
-        html,
-        body {
+        html, body {
             margin: 0;
             padding: 0;
             width: 100%;
             height: 100%;
             overflow-x: hidden;
         }
-
-        /* Reset some common elements that might affect full-width display */
-        body,
-        div,
-        section,
-        article,
-        header,
-        footer,
-        nav,
-        aside {
+        body, div, section, article, header, footer, nav, aside {
             box-sizing: border-box;
         }
-
-        /* Make sure the website content takes full width and height */
         .website-container {
             width: 100%;
             min-height: 100vh;
@@ -84,43 +66,30 @@ echo $htmlFinal;
         }
     </style>
     <?php if (!empty($website['css_personalizado'])): ?>
-        <style>
-            <?php echo $sanitized_css; ?>
-        </style>
+        <style><?php echo $sanitized_css; ?></style>
     <?php endif; ?>
     <?php if (!empty($website['css'])): ?>
-        <style>
-            <?php echo $website['css']; ?>
-        </style>
+        <style><?php echo $website['css']; ?></style>
     <?php endif; ?>
 </head>
-
 <body>
     <div class="website-container">
         <?php echo renderDynamicBlocks($website['conteudo'], $website['id'], $pdo); ?>
     </div>
-
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-            // Execute any scripts in the content
             document.querySelectorAll('script').forEach((el) => {
-                if (!el.src) { // Only execute inline scripts
+                if (!el.src) {
                     const newScript = document.createElement("script");
                     newScript.text = el.innerHTML;
                     document.body.appendChild(newScript);
                 }
             });
-
-            // Remove any residual Eyefind elements if they exist
             const eyefindElements = document.querySelectorAll('#eyefind-header, .eyefind-header');
             eyefindElements.forEach(el => el.remove());
-
-            // Make sure the body takes full width
             document.body.style.width = '100%';
             document.body.style.margin = '0';
             document.body.style.padding = '0';
-
-            // Ensure the website container fills the viewport
             const container = document.querySelector('.website-container');
             if (container) {
                 container.style.minHeight = '100vh';
@@ -129,5 +98,4 @@ echo $htmlFinal;
         });
     </script>
 </body>
-
 </html>
