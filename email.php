e82331c681a61da12d0a4205b7360d517e1eef5d
GabrielSam26 <gabrielsamdev@gmail.com>
2026-02-20 13:54:05 -0300

    chore: ajustar configurações do Jest

--- email.php
+++ email.php
@@ -131,7 +131,7 @@ if (isset($_GET['msg'])) {
     
     <!-- CKEditor - apenas se estiver visualizando um email -->
     <?php if ($emailVisualizado): ?>
-    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
     <?php endif; ?>
     
     <script>
