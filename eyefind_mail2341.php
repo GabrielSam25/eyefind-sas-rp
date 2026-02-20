41186a5aef53b40ca2b596a20ad8fdab308a9a7e
GabrielSam26 <gabrielsamdev@gmail.com>
2026-02-20 10:44:55 -0300

    feat: criar novo componente service

--- eyefind_mail2341.php
+++ eyefind_mail2341.php
@@ -46,7 +46,7 @@
                 <div class="flex flex-col md:flex-row items-center gap-6 w-full">
                     
                     <div class="w-64">
-                        <img src="" alt="Eyefind Logo" class="w-full">
+                        <img src="eyefindmail.png" alt="Eyefind Logo" class="w-full">
                     </div>
                 </div>
             </div>
